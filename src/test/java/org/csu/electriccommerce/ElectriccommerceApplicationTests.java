eadd23353994e7ed9d59e760c0e7b09ae24d06e7
Changxiaoying <331980235@qq.com>
2021-10-26 20:23:40 +0800

    init

---
+++ src/test/java/org/csu/electriccommerce/ElectriccommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.csu.electriccommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElectriccommerceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
