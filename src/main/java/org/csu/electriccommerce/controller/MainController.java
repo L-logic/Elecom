923f356f6ef3678e914ec45b5bb44d184faf280c
张琰 <1446174394@qq.com>
2021-11-10 03:30:32 +0800

    完成多关键字查询后端

--- src/main/java/org/csu/electriccommerce/controller/MainController.java
+++ src/main/java/org/csu/electriccommerce/controller/MainController.java
@@ -1,5 +1,6 @@
 package org.csu.electriccommerce.controller;
 
+import org.csu.electriccommerce.entity.Hunhe;
 import org.csu.electriccommerce.entity.Keyword;
 import org.csu.electriccommerce.tool.company.NewCleanFileClass;
 import org.csu.electriccommerce.tool.compkey.Algorithm;
@@ -9,7 +10,10 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
 import java.util.ArrayList;
 
 @Controller
@@ -23,13 +27,97 @@ public class MainController {
             Model model) {
         if (word != null) {
             System.out.println(word);
+            Keyword keyword = new Keyword();
+            keyword.setKeyword(word);
 
+            PathClass pa = new PathClass();
+            try {
+                NewCleanFileClass.Clean(pa.wordInNew, pa.wordOut,10);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            int num = 10;   //设定选取竞争关键字的个数
 
+            try {
+                new Algorithm().algorithm(keyword,num);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
 
-
-            model.addAttribute("");
+            ArrayList<Hunhe> arrayList = new ArrayList<>();
+            for (int i = 0; i < keyword.getMidkey().size(); i++) {
+                Hunhe hunhe = new Hunhe();
+                hunhe.setKeyword(keyword.getKeyword());
+                hunhe.setMidkey(keyword.getMidkey().get(i));
+                hunhe.setCompkey(keyword.getCompkey().get(i));
+                hunhe.setCompPower(keyword.getCompPoint().get(i));
+                arrayList.add(hunhe);
+            }
+            System.out.println(arrayList);
+            model.addAttribute(keyword);
+            model.addAttribute("arrayList",arrayList);
             return "page/listings";
         }
         return "index";
     }
+
+    @RequestMapping("/addAllKeyword")
+    public String addAllKeyword(HttpServletRequest req,Model model){
+//        System.out.println(count);
+//        ArrayList<Keyword> keywordArrayList = new ArrayList<>();
+//        for (int i = 0; i < count+1; i++) {
+//            Keyword keyword = new Keyword();
+//            if (i==0){
+//                keyword.setKeyword(req.getParameter("aword"));
+//                keywordArrayList.add(keyword);
+//            }
+//            String str = "aword"+i ;
+//            keyword.setKeyword(req.getParameter(str));
+//            keywordArrayList.add(keyword);
+//        }
+//        System.out.println(keywordArrayList);
+
+
+        System.out.println(req.getQueryString());
+        String[] list = req.getQueryString().split("&");
+        ArrayList<String> strings = new ArrayList<>();
+        for (int i = 0; i < list.length; i++) {
+            String str = list[i].split("=")[1];
+            String decodeFName = null;
+            try {
+                decodeFName = URLDecoder.decode(str, "utf-8");
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+            }
+            strings.add(decodeFName);
+        }
+        System.out.println(strings);
+
+        ArrayList<Keyword> keywordArrayList = new ArrayList<>();
+        for (int i = 0; i < strings.size(); i++) {
+            Keyword keyword = new Keyword();
+            keyword.setKeyword(strings.get(i));
+            keywordArrayList.add(keyword);
+        }
+        System.out.println(keywordArrayList);
+
+        PathClass pa = new PathClass();
+
+        try {
+            NewCleanFileClass.Clean(pa.wordInNew, pa.wordOut,10);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        int num = 10;   //设定选取竞争关键字的个数
+        for(int i = 0; i < keywordArrayList.size(); i++) {
+            try {
+                new Algorithm().algorithm(keywordArrayList.get(i),num);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        model.addAttribute("keywordArrayList",keywordArrayList);
+        return "index";
+    }
+
 }
