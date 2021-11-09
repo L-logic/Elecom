03348b65024c5c5dff4b4db40d2b7b0e0d4876ed
张琰 <1446174394@qq.com>
2021-11-09 18:30:10 +0800

    完成关键字展示

--- src/main/java/org/csu/electriccommerce/controller/MainController.java
+++ src/main/java/org/csu/electriccommerce/controller/MainController.java
@@ -1,5 +1,6 @@
 package org.csu.electriccommerce.controller;
 
+import org.csu.electriccommerce.entity.Hunhe;
 import org.csu.electriccommerce.entity.Keyword;
 import org.csu.electriccommerce.tool.company.NewCleanFileClass;
 import org.csu.electriccommerce.tool.compkey.Algorithm;
@@ -10,11 +11,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 @Controller
 @RequestMapping("/main")
 public class MainController {
-
 
     //新增种子关键字
     @RequestMapping("/addKeyword")
@@ -39,7 +40,19 @@ public class MainController {
             } catch (Exception e) {
                 e.printStackTrace();
             }
+
+            ArrayList<Hunhe> arrayList = new ArrayList<>();
+            for (int i = 0; i < keyword.getMidkey().size(); i++) {
+                Hunhe hunhe = new Hunhe();
+                hunhe.setKeyword(keyword.getKeyword());
+                hunhe.setMidkey(keyword.getMidkey().get(i));
+                hunhe.setCompkey(keyword.getCompkey().get(i));
+                hunhe.setCompPower(keyword.getCompPower().get(i));
+                arrayList.add(hunhe);
+            }
+            System.out.println(arrayList);
             model.addAttribute(keyword);
+            model.addAttribute("arrayList",arrayList);
             return "page/listings";
         }
         return "index";
---
+++ src/main/java/org/csu/electriccommerce/entity/Hunhe.java
@@ -0,0 +1,13 @@
+package org.csu.electriccommerce.entity;
+
+import lombok.Data;
+
+@Data
+public class Hunhe {
+
+    //纵向Keyword
+    private String keyword;
+    private String midkey;
+    private String compkey;
+    private Double compPower;
+}
