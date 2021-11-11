a285cbb70629b69178ec7771a177d9028233251b
张琰 <1446174394@qq.com>
2021-11-11 23:50:15 +0800

    遍历打分表

--- src/main/java/org/csu/electriccommerce/controller/GradeController.java
+++ src/main/java/org/csu/electriccommerce/controller/GradeController.java
@@ -4,8 +4,10 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.csu.electriccommerce.entity.CompkeyAndGrade;
 import org.csu.electriccommerce.entity.Grade;
+import org.csu.electriccommerce.entity.Hunhe;
 import org.csu.electriccommerce.entity.Rate;
 import org.csu.electriccommerce.service.GradeService;
+import org.csu.electriccommerce.service.MainService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -21,6 +23,8 @@ public class GradeController {
 
     @Autowired
     private GradeService gradeService;
+    @Autowired
+    private MainService mainService;
 
     @RequestMapping("/addGrade")
     public void addGrade(@RequestParam("value") float value , Model model ){
@@ -29,7 +33,9 @@ public class GradeController {
 
     @RequestMapping("/goTable")
     public String goTable(@RequestParam("word") String word,Model model){
+        ArrayList<Hunhe> arrayList = mainService.findComp(word);
         model.addAttribute("word",word);
+        model.addAttribute("arrayList",arrayList);
         return "page/table";
     }
 
